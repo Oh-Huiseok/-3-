be84df29520a5644cb5b4dcbb7f87c24fd15653b
Oh-Huiseok <rlatotquf45@naver.com>
2024-09-22 16:35:04 +0900

    chapter 4 까지

---
+++ src/main/java/me/ohheeseok/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,7 @@
+package me.ohheeseok.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
